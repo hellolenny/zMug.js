2194d8d303473880eb6240a78d963ede77187246
itsLennee <itslennee@gmail.com>
2021-04-01 20:02:06 +0200

    added non-minifed versione

--- README.md
+++ README.md
@@ -10,7 +10,11 @@ zMug.js takes its name from the "Indicator", **"HQIBPEXEZMUG".**, or ZeeMug/ZedM
 
 ## How does it work? (version 0.1)
 
-Your JSON file needs to use the following structure:
+zMug fetches the needed information from a JSON file, and builds all the indicated elements in a master parent (v.0.1). You can create multiple children in a single instance, for recursively call zMug and build the content.
+
+### Basic data structure
+
+Your JSON file needs to use the following object as base structure:
 
 ```
 {
@@ -25,7 +29,11 @@ Your JSON file needs to use the following structure:
 }
 ```
 
-All properties are optional **except** the two **MANDATORY: "name"** and **"type"**, which respectively are the element's ID and type/tag.
+### Properties.
+
+In order to understand what to build and which attributes to create, you need some generic properties that refers to some attributes, simply because their use is more common than others, or some other more specialized. zMug uses a syntax that can be quite flexible. In the same object you can create a single child, or multiple children.
+
+There are two **MANDATORY properties: "name"** and **"type"**, which respectively are the element's ID and type/tag.
 
 For example, I would like to create a div called "zMug". My JSON should be:
 
@@ -35,3 +43,120 @@ For example, I would like to create a div called "zMug". My JSON should be:
   "type": "div"
 }
 ```
+
+#### Optional properties.
+
+All other properties can be omitted or left with value: null. Let's delve into these:
+
+1. "class": nomen omen, add the declared class (or classes), for the corresponding element.
+2. "value": **used when "type" is a text tag (h1/h2/h3/h4/h5/h6/p) or img**. For text tags, "value" will be the text content, and for img the src.
+3. "strong": **for text type only** selected characters to add a strong tag.
+4. "attTyp" & "attVal": they need to be used together. "attTyp" defines the attribute, while "attVal" its value.
+5. "child": if this element has to have a/some child/children, zMug will be recursively called to create the necessary elements with "name" as parent.
+
+### Array values
+
+zMug gives you the option to send arrays as values for every property. Please note that, if you use an array to define multiple elements, you need to use arrays for every subsequent property.
+
+#### Example
+
+As a case in-point, I need to build two divs and a p. Your data should be something like this:
+
+1. "name" and "type": **name and value must have the same length**. You'll get a critical error if they don't match. If you need only one element
+
+  - In our example:
+
+  ```
+  {
+    "name": ["exampleDiv1","exampleDiv2", "exampleP"],
+    "type": ["div","div","p"],
+    ...
+  }
+  ```
+
+NOTE: "name", "type" and "value" cannot be arrays of arrays as value. The other properties, tho, can.
+
+2. "value": we need a string for our P. So, we'll have a null for our divs, and:
+
+  ```
+  {
+    "name": ["exampleDiv1","exampleDiv2", "exampleP"],
+    "type": ["div","div","p"],
+    "value": [null,null,"This is zMug. A JS builder!"],
+    ...
+  }
+  ```
+
+3. "class": every element can have a single class, multiple or none.
+
+  - Our first div doesn't have any classes (if so, you must use null, if you have multiple elements), the second has one, and our p has four classes.
+
+  ```
+  {
+    "name": ["exampleDiv1","exampleDiv2", "exampleP"],
+    "type": ["div","div","p"],
+    "value": [null,null,"This is zMug. A JS builder!"],
+    "class": [null,"classDiv2",["arrayClassP_1","arrayClassP_2","arrayClassP_3"]],
+    ...
+  }
+  ```
+
+4. "strong": if you need more than one string to get the strong tag, you need an array.
+
+  - Remember, only text tags can use "strong". So we need null for our divs:
+   ```
+  {
+    "name": ["exampleDiv1","exampleDiv2", "exampleP"],
+    "type": ["div","div","p"],
+    "value": [null,null,"This is zMug. A JS builder!"],
+    "class": [null,"classDiv2",["arrayClassP_1","arrayClassP_2","arrayClassP_3"]],
+    "strong": [null,null,["Hello","This","JS"]]
+    ...
+  }
+  ```
+
+5. "attTyp" and "attVal": if you need more attribute for the same element, **you need the type of container for attTyp and attVal**. 
+
+
+  - We want onmouseenter and onmouseleave on "exampleDiv1" and style on "exampleP":
+   ```
+  {
+    "name": ["exampleDiv1","exampleDiv2", "exampleP"],
+    "type": ["div","div","p"],
+    "value": [null,null,"This is zMug. A JS builder!"],
+    "class": [null,"classDiv2",["arrayClassP_1","arrayClassP_2","arrayClassP_3"]],
+    "strong": [null,null,["Hello","This","JS"]],
+    "attTyp": [["onmouseenter","onmouseleave"],null,"style"],
+    "attVal": [["function1()","function2()"],null,"width:200px;font-size:20px"]
+    ...
+  }
+  ```
+
+6. "child": if you have multiple elements, you can have a single object or an array of objects for each element as children.
+
+   ```
+  {
+    "name": ["exampleDiv1","exampleDiv2", "exampleP"],
+    "type": ["div","div","p"],
+    "value": [null,null,"This is zMug. A JS builder!"],
+    "class": [null,"classDiv2",["arrayClassP_1","arrayClassP_2","arrayClassP_3"]],
+    "strong": [null,null,["Hello","This","JS"]],
+    "attTyp": [["onmouseenter","onmouseleave"],null,"style"],
+    "attVal": [["function1()","function2()"],null,"width:200px;font-size:20px"]
+    "child": [[{
+      "name": "exampleDiv1Child1",
+      "type": "div"
+    },{
+      "name": "exampleDiv1Child2",
+      "type": "div"
+    }],
+    {
+      "name": "exampleDiv2Child1",
+      "type": "div"
+    }]
+  }
+  ```
+
+### Child and alternative types
+
+### Goals
---
+++ dist/v1/zMug.js
@@ -0,0 +1,280 @@
+/* zMug - aux */
+
+var inf = {
+  sel(c,n){
+      var idreg = /#/g,
+      classreg = /\./g,
+      res,san;
+    if (c.match(idreg) == "#") {
+        san = c.replace(idreg,'');
+        res=document.getElementById(san);
+      return res;
+    } else if (c.match(classreg) == ".") {
+          san = c.replace(classreg,'');
+          classcount = document.getElementsByClassName(san);
+        if (typeof n != "number"){
+          return classcount;
+        } else {
+          res=classcount.item(n);
+          return res;
+        }
+    } else {
+          tagcount = document.getElementsByTagName(c);
+        if (typeof n != "number"){
+            return tagcount;
+        }
+          res=tagcount.item(n);
+        return res;     
+    }
+  }, 
+},
+att = {
+  giveAtt(parent, att, val,n){
+    var tAtt = document.createAttribute(att);
+    tAtt.value = val;
+    inf.sel(parent,n).setAttributeNode(tAtt);
+  },
+  addCl(parent, clname,n){
+    inf.sel(parent,n).classList.add(clname);
+  },
+  strong(parent,select,n){
+    var toReg = select,
+    paDiv = inf.sel(parent,n),
+    origStr= paDiv.innerHTML,
+    strStr = origStr.replace(toReg, "<strong>"+toReg+"</strong>");
+    paDiv.innerHTML = strStr;
+  }
+},
+
+build = {
+  el(parent, type, id,n){
+    var tEl = document.createElement(type),
+    paDiv = inf.sel(parent,n);
+    if(typeof id != 'undefined') {
+      tEl.id = id;
+    }
+    paDiv.append(tEl);
+  },
+  text(parent,type,txtCont,id,n)
+  { var tText = document.createElement(type),
+    paDiv = inf.sel(parent,n);
+    if(typeof id != 'undefined') {
+      tText.id = id;
+    }
+    tText.textContent = txtCont;
+    paDiv.append(tText);
+  },
+  img(parent,src,id,n){
+    var tImg = document.createElement("img"),
+    paDiv = inf.sel(parent,n);
+    if(typeof id != 'undefined') {
+      tImg.id = id;
+    }
+    tImg.src = src;
+    paDiv.append(tImg);
+  }
+};
+
+
+/* zMug - builder */
+
+var zmug = {
+  Call(parent,url){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var jsonRes=JSON.parse(this.responseText);
+          if(jsonRes!=undefined){
+            zmug.Build(parent,jsonRes);
+          }
+          }};
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  },
+  Build(parent,response){
+    var pa = parent,
+        r = response;
+
+    if(r.length==undefined){
+      r = new Array(r);
+    }
+    
+    for (let tr = 0; tr < r.length; tr++) {
+      var id = r[tr].name,
+      typ = r[tr].type,
+      cl = r[tr].class,
+      vl = r[tr].value,
+      st = r[tr].strong,
+      aT = r[tr].attrTyp,
+      aV = r[tr].attrVal,
+      ch = r[tr].child;
+
+      if(typeof id===typeof typ){
+        if(typeof id ==="string"){
+          zmug.Element(pa, id,typ,vl,st);
+        } else if(id.length===typ.length){
+          if(typeof id ==="object"){
+            for (let tid = 0; tid < id.length; tid++) {
+              var strong=undefined, 
+                  value=undefined;
+
+              if(vl==undefined){
+                if((typ[tid]=="img")||(typ[tid]=="h1")||(typ[tid]=="h2")||(typ[tid]=="h3")||(typ[tid]=="h4")||(typ[tid]=="h5")||(typ[tid]=="p")){
+                  console.log("Critical error: value for "+id[tid]+ "which is a "+typ[tid]+"is: "+vl+".");
+                }
+              }else{
+                if(typeof vl == "object"){
+                  if(vl!==null){
+                    value = vl[tid];
+                  } 
+                } else if(typeof vl == "string"){
+                  value = vl;
+                }
+              }
+
+            if(st!=undefined){  
+              if(typeof id == "object"){
+                if(typeof st == "object"){
+                  strong = st[tid];
+                } else if(typeof st == "string"){
+                  strong = st;
+                }
+              }
+            }
+              zmug.Element(pa, id[tid],typ[tid],value,strong);
+            }
+          } 
+        } else {
+          console.log("Error: id count: "+id.length+" is not equal to the element count: "+typ.length);
+        }
+      } else {
+          console.log("Error: typeof id: "+typeof id+" is not equal to the typeof elements: "+typeof typ);
+      }
+
+      /* inject class */
+
+      if((cl!=undefined)&&(cl!=null)){
+        if(typeof id == "object"){
+          for (let tcl0 = 0; tcl0 < id.length; tcl0++) {
+            if(typeof cl[tcl0] == "string"){
+              att.addCl("#"+id[tcl0],cl[tcl0]);
+            } else if(cl[tcl0]!=null){
+              for (let tcl3 = 0; tcl3 < id.length; tcl3++) {
+                att.addCl("#"+id[tcl0],cl[tcl0][tcl3]);
+              }
+            }
+          }
+        } else if(typeof id == "string"){
+          if(typeof cl == "object"){
+              for (let tcl1 = 0; tcl1 < cl.length; tcl1++) {
+                  if(cl[tcl1]!=null){
+                    att.addCl("#"+id,cl[tcl1]);
+                  }
+                }
+              } else {
+                att.addCl("#"+id,cl);
+          }
+        }
+      }
+
+      /* inject attribute */
+
+      if((aT!=null)&&(aV!=null)&&((aT!=undefined)&&(aV!=undefined))){
+        if(typeof id == "object"){
+          for (let tao = 0; tao < id.length; tao++) {
+            if((aT[tao]!=null)&&(aV[tao]!=null)){
+              if((typeof aT[tao] == "object")&&(typeof aV[tao] == "object")&&(aT[tao].length==aV[tao].length)){
+                for (let tat2 = 0; tat2 < aT[tao].length; tat2++) {
+                  if((aT[tao][tat2]!=null)&&(aV[tao][tat2]!=null)){
+                    att.giveAtt("#"+id[tao],aT[tao][tat2],aV[tao][tat2]);
+                  }
+                }
+              } else {
+                att.giveAtt("#"+id[tao],aT[tao],aV[tao]);
+              }
+            }
+            }
+        } else if(typeof id == "string"){
+          if((typeof aT == "object")&&(typeof aV == "object")&&(aT.length==aV.length)){
+            for (let tat = 0; tat < aT.length; tat++) {
+              if((aT[tat]!=null)&&(aV[tat]!=null)){
+                att.giveAtt("#"+id,aT[tat],aV[tat]);
+              }
+            }
+          } else {
+            att.giveAtt("#"+id,aT,aV);
+          }
+        }
+      }
+
+      if((ch!=undefined)&&(ch!=null)){
+        if((typeof id == "string")&&(ch.length==undefined)) {
+            zmug.Build(id,ch);
+        } else if ((typeof id == "string")&&(ch.length!=undefined)){
+          for (let tch0 = 0; tch0 < ch.length; tch0++) {
+            zmug.Build(id,ch[tch0]);
+          }
+        } else if((typeof id == "object")&&(ch.length!=undefined)){
+              for (let tch2 = 0; tch2 < id.length; tch2++) {
+                  if((ch[tch2]!=null)&&(ch[tch2]!=undefined)){
+                    if(ch[tch2].length!=undefined){
+                      for (let tch3 = 0; tch3 < ch[tch2].length; tch3++) {
+                        zmug.Build(id[tch2],ch[tch2][tch3]);
+                      }
+                    } else if(ch[tch2].length==undefined){
+                      zmug.Build(id[tch2],ch[tch2]);
+                    }
+                  }
+              }
+          }
+      }
+    }
+  },
+  Element(parent,id,type,value,strong){
+    switch (type) {
+      case "h1":
+      case "h2":
+      case "h3":
+      case "h4":
+      case "h5":
+      case "p":
+        if(value!=undefined){
+          build.text("#"+parent,type,value,id);
+        }
+        if(strong!=undefined){
+          if(typeof strong==="string"){
+            att.strong("#"+id,strong)
+          }
+          if(typeof strong==="object"){
+            for (let tst = 0; tst < strong.length; tst++) {
+              att.strong("#"+id,strong[tst]);
+            }
+          }
+        }
+        break;
+      case "CTA":
+        if(value!=undefined){
+          build.el("#"+parent,"a",id);
+          build.el("#"+id,"div",id+"Main");
+          build.text("#"+id+"Main","h4",value,id+"h4");
+          build.el("#"+id,"div",id+"Dec");
+        }
+        break;
+      case "img":
+        if(value!=undefined){
+          build.img("#"+parent,value,id);
+        }
+        break;
+      default:
+        build.el("#"+parent,type,id);
+        break;
+  }
+}
+}
+
+/* zMug - call funct */
+
+function zMug(parent,url){
+  zmug.Call(parent,url);
+}
+
